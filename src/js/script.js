f2318fb20099d24e83669d316c0fca38505a2f8e
Jamie Lee <jamiestevelee@gmail.com>
2021-04-06 22:54:29 +0100

    Add styles and js, Zaire and Yemen

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,26 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		message: 'test'
-	}
-})
+		windowHeight: '100vh',
+		nationInfo,
+		message: 'test',
+		currentNation: false
+	},
+	methods: {
+		getNationInfo(name) {
+			this.currentNation = this.nationInfo[name];
+		},
+		getWindowHeight(event) {
+			this.windowHeight = window.innerHeight + 'px';
+		},
+	},
+	mounted() {
+		this.$nextTick(function() {
+			window.addEventListener('resize', this.getWindowHeight);
+			this.getWindowHeight();
+		});
+	},
+	beforeDestroy() {
+		window.removeEventListener('resize', this.getWindowHeight);
+	},
+});
